8b725604ee699d7828c3a791f712ef073a91ba01
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-17 00:17:21 -0700

    Done
    All done

--- src/BigONotation.java
+++ src/BigONotation.java
@@ -1,4 +1,29 @@
 
+
+/*
+ * Answers to the last two questions:
+ * 1) BubbleSort:
+ * 	-- > f(x) = n * n;
+ *	--> g(x) = n^2
+ * 
+ * 2) 
+ * 
+ * 
+ * Answers to Part I:
+ * 
+ * 1) Answer: Log(n)
+ * 
+ * 2) Answer: n^4
+ * 
+ * 3) Answer: n^3
+ * 
+ * 4) Answer: n^5
+ * 
+ * 5) Answer: n^2
+ * 
+ * 6) Answer: n log(n)
+ * 
+ * */
 public class BigONotation {
 
 	public static void main(String[] args) {
@@ -8,15 +33,17 @@ public class BigONotation {
 
 	/*
 	 * Number of operations = 1;
+	 * f(x) = 1; c == 0, k = >-1 && >1
 	 * O(1)
 	 * */
 	public static boolean isEmpty() {
 		return head == null;
 	}
 
-
 	/*
-	 * 
+	 * Number of operations = 4;
+	 * f(x) = c + 2n, c >= 1, k >= 1
+	 * O(2n)
 	 * */
 	public static int num_occurrences(int n) {
 		int count = 0;
@@ -29,5 +56,32 @@ public class BigONotation {
 			}
 		}
 		return count;
-	}	
+	}
+
+	/*
+	 * Number of operations = 2;
+	 * f(x) = n + n, c <= infinity?, k >= 1
+	 * O(2n)
+	 * */
+	public static void c(int n) { //three loops
+		for(int a = 0; a < n; a++) {
+			System.out.println( a * a);
+		}
+		num_occurrences(n);
+	}
+
+	/*
+	 * Number of operations = 5?
+	 * f(x) = n, c <= ??? what is C?, k >= 3
+	 * O(n)
+	 * */
+	public static boolean isPrime(int n) {
+		if(n == 1) return false;
+		for(int i = 2; i <n; i++) {
+			if( n % i == 0 ) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
